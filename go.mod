96135672b06b3e3d52b8da84af2e3973f167fded
iqbaltk <iqbaltri156@gmail.com>
2022-06-15 13:24:21 +0700

    fix v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/iqbaltk/go-kalkulator
+module github.com/iqbaltk/go-kalkulator/v2
 
 go 1.18
--- matematika.go
+++ matematika.go
@@ -1,9 +1,13 @@
 package matematika
 
-func CekGanjilGenap(number int) string {
-	if number%2 == 0 {
-		return "Genap"
-	} else {
-		return "Ganjil"
+func CekGanjilGenap(number ...int) []string {
+	var data []string
+	for _, i := range number {
+		if i%2 == 0 {
+			data = append(data, "Genap")
+		} else {
+			data = append(data, "Ganjil")
+		}
 	}
+	return data
 }
